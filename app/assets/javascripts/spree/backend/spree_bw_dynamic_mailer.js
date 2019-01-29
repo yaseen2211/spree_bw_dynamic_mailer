a7000200a6d64c1725c13b5ab88df80c363aec61
yaseen iqbal <p136011@nu.edu.pk>
2019-01-29 20:44:08 +0500

    finalize dynamic email extension

--- app/assets/javascripts/spree/backend/spree_bw_dynamic_mailer.js
+++ app/assets/javascripts/spree/backend/spree_bw_dynamic_mailer.js
@@ -1,2 +1,23 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
+//= require ckeditor/init
+
+$(document).ready(function()
+{
+  var navItems = $('.admin-menu li > a');
+  var navListItems = $('.admin-menu li');
+  var allWells = $('.admin-content');
+  var allWellsExceptFirst = $('.admin-content:not(:first)');
+  
+  allWellsExceptFirst.hide();
+  navItems.click(function(e)
+  {
+      e.preventDefault();
+      navListItems.removeClass('active');
+      $(this).closest('li').addClass('active');
+      
+      allWells.hide();
+      var target = $(this).attr('data-target-id');
+      $('#' + target).show();
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Spree::Core::Engine.add_routes do
   namespace :admin do
     resources :email_contents
   end
+  mount Ckeditor::Engine => '/ckeditor'
 end
