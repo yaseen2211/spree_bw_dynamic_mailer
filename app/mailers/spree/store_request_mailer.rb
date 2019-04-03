84f8ce1d14a573fe393f174726d5b7ccd53a9142
yaseen iqbal <yaseeniqbal119@gmail.com>
2019-04-03 19:34:58 +0500

    Add emails for vendor

--- app/mailers/spree/store_request_mailer.rb
+++ app/mailers/spree/store_request_mailer.rb
@@ -2,6 +2,11 @@ class Spree::StoreRequestMailer < ApplicationMailer
 
 	def store_request_thankyou(store_request)
 		@request_info = store_request
-		mail(to: "omsolutionpk@gmail.com",cc:'p136011@nu.edu.pk', subject: "Vendor Registeration Request.")
+		mail(to: "omsolutionpk@gmail.com", subject: "Vendor Registeration Request.")
+	end
+
+	def response_to_owner(store_request)
+		@request_info = store_request
+		mail(to: @request_info. contact_email_address , subject: "Store Request Response")
 	end
 end
--- app/mailers/spree/user_mailer_decorator.rb
+++ app/mailers/spree/user_mailer_decorator.rb
@@ -1,4 +1,6 @@
 Spree::UserMailer.class_eval do
+
+
   def welcome(user)
     @user = user
     finder_obj = Spree::Email::EmailDynamic.finder("welcome","Spree::User")
@@ -8,7 +10,17 @@ Spree::UserMailer.class_eval do
     @single_replace_temp = Spree::Email::EmailDynamic.make_dynamic([@user],finder_obj.template)
     mail(to: @user.email, from: from_address, subject: subject)
   end
+
+
+  def vendor_welcome(user, token)
+    @vendor_password_reset_url = spree.edit_spree_user_password_url(reset_password_token: token, host: Spree::Store.current.url)
+    @user = user
+    mail(to: @user.email, from: from_address, subject: "Request for Change Account Credentials")
+  end
+
+
 end
+
 
 
 
