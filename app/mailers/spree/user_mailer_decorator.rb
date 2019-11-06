1e91c6fdc1bc5f916ff4679486404549dfdb0829
usmanriaz111 <l124061@lhr.nu.edu.pk>
2019-11-06 21:06:08 +0500

    Updated the reviews

--- app/mailers/spree/user_mailer_decorator.rb
+++ app/mailers/spree/user_mailer_decorator.rb
@@ -10,7 +10,29 @@ Spree::UserMailer.class_eval do
     social_user_name  =  @user.email.present? ? @user.email.split("@").first : ""
     (@user.first_name.blank? || @user.last_name.blank?) ? (@user.first_name = social_user_name) : "" 
     @single_replace_temp = Spree::Email::EmailDynamic.make_dynamic([@user],finder_obj.template)
-    mail(to: @user.email, from: from_address, subject: "Welcome to Black-Line Your Number One Abaya Destination", bcc: BCC_EMAILS)
+    mail(to: @user.email, from: from_address, subject: "Black-Line, Welcome to Black-Line Your Number One Abaya Destination", bcc: BCC_EMAILS)
+  end
+
+  def inform_admin_review(user,product)
+    @user = user
+    user_email = @user.email.present? ? @user.email: "omsolutionpk@gmail.com"
+    if !Rails.env.production?
+        user_email = "omsolutionpk@gmail.com"
+    end
+    @product = product
+    I18n.locale = :en
+    mail(to: user_email, from: from_address, subject: "A user made a review on product "+product.name)
+  end
+
+  def inform_user_approve_review(user,product)
+    @user = user
+    user_email = @user.email.present? ? @user.email: "omsolutionpk@gmail.com"
+    if !Rails.env.production?
+      user_email = "omsolutionpk@gmail.com"
+    end
+    @product = product
+    I18n.locale = :en
+    mail(to: user_email, from: from_address, subject: "your review has been approved on "+product.name+" by admin")
   end
 
 
