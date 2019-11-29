495dd4e7952a66ef1342164b89b37b08b0516943
Yaseen Iqbal <p136011@nu.edu.pk>
2019-11-29 16:17:56 +0500

    code removal w.r.t tim auction

--- app/mailers/spree/user_mailer_decorator.rb
+++ app/mailers/spree/user_mailer_decorator.rb
@@ -3,13 +3,9 @@ Spree::UserMailer.class_eval do
   def welcome(user)
     @user = user
     finder_obj = Spree::Email::EmailDynamic.finder("welcome","Spree::User")
-    # finder_obj.translations.inspect
-    # I18n.locale = :en
     subject = finder_obj.subject
-    social_user_name  =  @user.email.present? ? @user.email.split("@").first : ""
-    (@user.first_name.blank? || @user.last_name.blank?) ? (@user.first_name = social_user_name) : "" 
     @single_replace_temp = Spree::Email::EmailDynamic.make_dynamic([@user],finder_obj.template)
-    mail(to: @user.email, from: from_address, subject: "Welcome to Tim Auction", bcc: BCC_EMAILS)
+    mail(to: @user.email, from: from_address, subject: "Welcome to Tim Auction")
   end
 
   def vendor_welcome(user, token)
