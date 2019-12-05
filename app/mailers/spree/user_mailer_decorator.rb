257662f60dfb46d6981a1e89e3ce326c139edc79
Yaseen Iqbal <p136011@nu.edu.pk>
2019-12-05 18:11:43 +0500

    user welcome email template change

--- app/mailers/spree/user_mailer_decorator.rb
+++ app/mailers/spree/user_mailer_decorator.rb
@@ -1,20 +1,18 @@
 Spree::UserMailer.class_eval do
 
-  def welcome(user)
+  def welcome(user,token)
     @user = user
     finder_obj = Spree::Email::EmailDynamic.finder("welcome","Spree::User")
     subject = finder_obj.subject
     @single_replace_temp = Spree::Email::EmailDynamic.make_dynamic([@user],finder_obj.template)
+    @vendor_password_reset_url = spree.edit_spree_user_password_url(reset_password_token: token, host: Spree::Store.current.url)
     mail(to: @user.email, from: from_address, subject: "Welcome to Tim Auction")
   end
-
   def vendor_welcome(user, token)
     @vendor_password_reset_url = spree.edit_spree_user_password_url(reset_password_token: token, host: Spree::Store.current.url)
     @user = user
     mail(to: @user.email, from: from_address, subject: "Tim Auction, Request for Change Account Credentials", bcc: BCC_EMAILS)
   end
-
-
 end
 
 
