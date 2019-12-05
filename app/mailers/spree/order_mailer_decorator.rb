adf01d857cfa7b3cbe6c2996480ace123ecd1f10
Yaseen Iqbal <p136011@nu.edu.pk>
2019-12-05 18:30:28 +0500

    change class to spree 6 syntax

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,16 +1,16 @@
-Spree::OrderMailer.class_eval do
-	 
-  def confirm_email(order, resend = false)
-    @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
-    finder_obj = Spree::Email::EmailDynamic.finder("order-complete","Spree::Order")
-    finder_obj.translations.inspect
-    I18n.locale = :en
-    subject = finder_obj.subject
-    single_replace_temp = Spree::Email::EmailDynamic.make_dynamic([@order],finder_obj.template)
-    partial_hash = {"order_details" => {:order => @order}}
-    @replaced_temp =  Spree::Email::EmailDynamic.detailed_dynamic(partial_hash, single_replace_temp,'/spree/order_mailer/')
+module Spree
+  module OrderMailerDecorator
+    def confirm_email(order, resend = false)
+      @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
+      finder_obj = Spree::Email::EmailDynamic.finder("order-complete","Spree::Order")
+      finder_obj.translations.inspect
+      I18n.locale = :en
+      subject = finder_obj.subject
+      single_replace_temp = Spree::Email::EmailDynamic.make_dynamic([@order],finder_obj.template)
+      partial_hash = {"order_details" => {:order => @order}}
+      @replaced_temp =  Spree::Email::EmailDynamic.detailed_dynamic(partial_hash, single_replace_temp,'/spree/order_mailer/')
 
-    mail(to: @order.email, from: from_address, subject: "Black-Line Order Confirmation #{@order.number}", bcc: BCC_EMAILS )
+      mail(to: @order.email, from: from_address, subject: "Black-Line Order Confirmation #{@order.number}", bcc: BCC_EMAILS )
+    end
   end
-
 end
