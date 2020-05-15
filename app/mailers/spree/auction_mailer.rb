ae16f76533b3f6e330a2c4331a7541d7c6351462
yaseen iqbal <yaseeniqbal119@gmail.com>
2020-05-15 12:17:31 +0500

    feat: Add win auction mailer for winner

--- app/mailers/spree/auction_mailer.rb
+++ app/mailers/spree/auction_mailer.rb
@@ -5,5 +5,10 @@ class Spree::AuctionMailer < ApplicationMailer
 		@auction       = auction
 		mail(to: @vendor_email , subject: "Sold Auction Notification.")
 	end
+	def sold_notification_to_winner(auction,winner_email)
+		@winner_email = winner_email
+		@auction       = auction
+		mail(to: @winner_email , subject: "Win Auction Notification.")
+	end
 
 end
