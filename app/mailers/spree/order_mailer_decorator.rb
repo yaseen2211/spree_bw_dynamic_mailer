c7dd6539aba3c673a80685d63ecec43ac4d02341
Haseeb Waqas <haseebwaqas016@gmail.com>
2020-06-12 16:24:26 +0500

    feat(mail-templates): make dynamic mail template for confirm email to vendor and user.

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,20 +1,32 @@
 module Spree
   module OrderMailerDecorator 
     def confirm_email(order, resend = false)
+      @user = order.user
+      @auction = order.auction
+      @order_shipping_address = order.shipping_address
+      @user_address = order.user.shipping_address
+      @product = @auction.variant.product
+
       @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
       subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
       subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.confirm_email.subject')} ##{@order.number}"
-      mail(to: @order.email, from: from_address, subject: 'CONGRATULATIONS!! {{first_name}}, ORDER {{Order Number}} Confirmed')
+      mail(to: @order.email, from: from_address, subject: "CONGRATULATIONS! #{@user.first_name}, ORDER #{@order.number} Confirmed")
     end
 
     def confirm_email_to_vendor(order,resend = false)
+      @auction = order.auction
+      @order_shipping_address = order.shipping_address
+      @user_address = order.user.shipping_address
+      @product = @auction.variant.product
+
       @order  = order.respond_to?(:id) ? order : Spree::Order.find(order)
       subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
       # subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.confirm_email.subject')} ##{@order.number}"
-      subject += "CONGRATULATIONS!! {{Vendor_name}}, ORDER {{Order Number}} Confirmed"
+      
       
       @vendor        = @order.auction.present? ? @order.auction.vendor : ''
       @vendor_email  = @vendor.present? ? @vendor.user.email : ''
+      subject += "CONGRATULATIONS!! #{@vendor.name}, ORDER #{@order.number} Confirmed"
       mail(to: @vendor_email, from: from_address, subject: subject)
     end
 
